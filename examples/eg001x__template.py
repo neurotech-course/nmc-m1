f73070f9da89a8475ff39ea69997b24e776c86ec
John Griffiths <j.davidgriffiths@gmail.com>
2022-05-30 15:10:35 -0400

    examples folder

---
+++ examples/eg001x__template.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+r"""
+=================================
+Example script demo
+=================================
+
+
+"""  
+
+# sphinx_gallery_thumbnail_number = 1
+#
+# %%
+# Importage
+# --------------------------------------------------
+
+# Local code tools and related imports
+# from thisib import *
+
+# Some neuroimaging library things
+import nibabel as nib
+from nilearn.plotting import plot_stat_map
+from nilearn.image import index_img
+
+# Some generic things
+from matplotlib import pyplot as plt
+import os
+import pandas as pd
+
+
+# %%
+# Some thing
+# --------------------------------------------------
+#
+# Say a thing
+# blah
+# blah blah
+#
+
+athing = '.'
+
+# %%
+# Do a thing
+# --------------------------------------------------
+
+print('A thing')
+
+
+# %%
+# Do another thing
+# ---------------------------------------------------
+#
+# The thing does a thing like so:
+# 1. Stuff
+# 2. More stuff
+#
+# And then more things
+
+print('Another thing')
+
+
